8a36367d03eb1bfad9e9d446a541d480c87094f7
Jackson-Vieira <jacksonvieiira12@gmail.com>
2024-02-05 06:53:45 -0300

    feat: add schedule create tip

--- backend/src/tip_generator/handler.py
+++ backend/src/tip_generator/handler.py
@@ -1,18 +1,18 @@
-import logging
+import os
 import asyncio
+import boto3
 
-from openai import AsyncOpenAI
+from datetime import date
+from openai import AsyncOpenAI, OpenAIError
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+from botocore.exceptions import ClientError
 
+import time
 
 def get_secret():
     return "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"
 
-
 OPENAI_API_KEY = get_secret()
-client = AsyncOpenAI(api_key=OPENAI_API_KEY)
 
 PROMPT_SYSTEM = """
 Act as a programming language tip expert generator for curios peoples
@@ -27,33 +27,64 @@ JSON expected keys: key_tip, description and code_example
 include all necessary imports   the example and use code best practices.
 """
 
+def put_tip(language, tip):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE_NAME'])
+    try:
+        response = table.put_item(
+            Item={
+                'tip_language': language,
+                'tip_date': str(date.today()),
+                'tip': tip
+            }
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    return response
 
 async def generate_tip(language: str):
-    response = await client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        frequency_penalty=0.2,
-        presence_penalty=0.2,
-        temperature=0.5,
-        top_p=0.5,
-        max_tokens=1200,
-        stop=None,
-        messages=[
-            {
-                "role": "system",
-                "content": PROMPT_SYSTEM,
-            },
-            {"role": "user", "content": PROMPT_USER_TEMPLATE + language},
-        ],
-    )
-    result = response.choices[0].message.content
-    logger.info(f"Tip Result: {result}")
-    return result
+    client = AsyncOpenAI(api_key=OPENAI_API_KEY)
 
+    result = None
+    try:
+        response = await client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            frequency_penalty=0.2,
+            presence_penalty=0.2,
+            temperature=0.5,
+            top_p=0.5,
+            max_tokens=1200,
+            timeout=15,
+            stop=None,
+            messages=[
+                {
+                    "role": "system",
+                    "content": PROMPT_SYSTEM,
+                },
+                {"role": "user", "content": PROMPT_USER_TEMPLATE + language},
+            ],
+        )
+        result = response.choices[0].message.content.strip()
+    except OpenAIError as e:
+        print(f"Error: {e}")
+        return None
+    finally:
+        await client.close()
+    return language, result
 
 async def generate_tips():
-    results = await asyncio.gather(generate_tip("Python"))
-    logger.info(f"Tip Results {results}")
-
+    return await asyncio.gather(generate_tip("Python"), generate_tip("Javascript"), generate_tip("Go"))
 
 def run(event, context):
-    asyncio.run(generate_tips())
+    start_time = time.time()
+    print("Start processing tips")
+
+    tips = asyncio.run(generate_tips())
+
+    print(f"Results: {tips}")
+    for language, tip in tips:
+        put_tip(language, tip)
+
+    end_time = time.time()
+    total_time = end_time - start_time
+    print(f"Finish processing tips {total_time}s")
