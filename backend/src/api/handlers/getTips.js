0f84f57c97153284ac207c1ab2ea62a0c0dd0b99
Jackson-Vieira <jacksonvieiira12@gmail.com>
2024-02-08 06:10:12 -0300

    filter weekly tips

--- backend/src/api/handlers/getTips.js
+++ backend/src/api/handlers/getTips.js
@@ -1,15 +1,28 @@
 import { DynamoDBClient} from "@aws-sdk/client-dynamodb";
 import { ScanCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
+function getLastSunday(){
+    const currentDate = new Date();
+    const currentDayOfWeek = currentDate.getDay(); // 0 Domingo, 1 Segunda, ...
+    const daysToSubtract = (currentDayOfWeek + 7) % 7;
+    const lastSundayDate = new Date(currentDate);
+    lastSundayDate.setDate(currentDate.getDate() - daysToSubtract);
+    return lastSundayDate;
+};
+
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
 async function getTips(event){
 
+    const lastSundayFormated = getLastSunday().toISOString().split("T")[0];
+
     const command = new ScanCommand({
         TableName: process.env.DYNAMODB_TABLE_NAME,
-        Select: "ALL_ATTRIBUTES",
-    });
+        FilterExpression: "tip_date >= :lastSunday",
+        ExpressionAttributeValues: {
+             ":lastSunday": lastSundayFormated
+    }});
 
     const response = await docClient.send(command);
     const tips = response.Items;
